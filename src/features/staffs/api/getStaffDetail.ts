4e78c18e797d80c77f14c9bd34012063033c5873
helitoo <bao162006@gmail.com>
2026-07-15 17:46:43 +0700

    Update getStaffDetail API

--- src/features/staffs/api/getStaffDetail.ts
+++ src/features/staffs/api/getStaffDetail.ts
@@ -2,5 +2,5 @@ import { axiosClient, getResponseData } from "../../../lib/axios";
 import type { StaffDetail } from "../types";
 
 export const getStaffDetail = async (staffId: number): Promise<StaffDetail> => {
-    return getResponseData(axiosClient.get<StaffDetail>(`/admin/staff/${staffId}`));
+    return getResponseData(axiosClient.get<StaffDetail>(`/api/Admin/users/${staffId}`));
 };
