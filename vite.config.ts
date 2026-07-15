9c275e80f79ff350d8a2fabafb8c9b65a3eb7aee
helitoo <bao162006@gmail.com>
2026-07-15 11:05:57 +0700

    Config proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://stagingapi.catspeak.com.vn',
+      "/api": {
+        target: "https://admin-staging-api.catspeak.com.vn/",
         changeOrigin: true,
         secure: true,
       },
     },
   },
-})
+});
