e44e83cec4fadd1c8becf18487177b70e6e01fd4
helitoo <bao162006@gmail.com>
2026-07-15 17:50:52 +0700

    Update Auth APIs

--- src/features/auth/api/forgotPassword.ts
+++ src/features/auth/api/forgotPassword.ts
@@ -9,17 +9,17 @@ import type {
 export const sendForgotPasswordOtp = async (
   data: ForgotPasswordRequest,
 ): Promise<void> => {
-  return getResponseData(axiosClient.post("/Auth/forgot-password", data));
+  return getResponseData(axiosClient.post("/api/Auth/forgot-password", data));
 };
 
 export const verifyResetOtp = async (
   data: VerifyResetOtpRequest,
 ): Promise<VerifyResetOtpResponse> => {
-  return getResponseData(axiosClient.post("/Auth/verify-reset-otp", data));
+  return getResponseData(axiosClient.post("/api/Auth/verify-reset-otp", data));
 };
 
 export const resetPassword = async (
   data: ResetPasswordRequest,
 ): Promise<void> => {
-  return getResponseData(axiosClient.post("/Auth/reset-password", data));
+  return getResponseData(axiosClient.post("/api/Auth/reset-password", data));
 };
